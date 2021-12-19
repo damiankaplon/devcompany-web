d7000ebe0a1ccd2a8bf901e607f81268d253790a
Damian <kaplon.damian99@gmail.com>
2021-12-19 17:29:55 +0100

    Redesing home view

--- src/main/java/pl/damiankaplon/devcompanyweb/controller/UserController.java
+++ src/main/java/pl/damiankaplon/devcompanyweb/controller/UserController.java
@@ -1,16 +1,32 @@
 package pl.damiankaplon.devcompanyweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import pl.damiankaplon.devcompanyweb.model.Client;
+import pl.damiankaplon.devcompanyweb.service.ClientService;
 
 import java.security.Principal;
 
 @Controller
 public class UserController {
+
+    private final ClientService clientService;
+
+    public UserController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/")
+    public String defaultView(Principal principal, Model model) {
+        return home(principal, model);
+    }
+
     @GetMapping("/home")
-    public String login(){
+    public String home(Principal principal, Model model){
+        Client client = (Client) clientService.findByUsername(principal.getName());
+        model.addAttribute("client", client);
         return "home";
     }
 }
