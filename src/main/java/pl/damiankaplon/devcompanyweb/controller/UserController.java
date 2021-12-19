136bfd5121700c64e9410349be0b210ec3839d68
Damian <kaplon.damian99@gmail.com>
2021-12-19 19:38:26 +0100

    Add view for listing sales for logged in client

--- src/main/java/pl/damiankaplon/devcompanyweb/controller/UserController.java
+++ src/main/java/pl/damiankaplon/devcompanyweb/controller/UserController.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.damiankaplon.devcompanyweb.model.Client;
+import pl.damiankaplon.devcompanyweb.model.Sale;
 import pl.damiankaplon.devcompanyweb.service.ClientService;
+import pl.damiankaplon.devcompanyweb.service.SaleService;
+import pl.damiankaplon.devcompanyweb.service.exception.NoClientsFound;
+import pl.damiankaplon.devcompanyweb.service.exception.NotSpecifiedReqArgs;
 
 import java.security.Principal;
 
@@ -13,14 +17,23 @@ import java.security.Principal;
 public class UserController {
 
     private final ClientService clientService;
+    private final SaleService saleService;
 
-    public UserController(ClientService clientService) {
+    public UserController(ClientService clientService, SaleService saleService) {
         this.clientService = clientService;
+        this.saleService = saleService;
     }
 
     @GetMapping("/")
     public String defaultView(Principal principal, Model model) {
         return home(principal, model);
+    }
+
+    @GetMapping("/list-invoices.html")
+    public String listInvoices(Principal principal, Model model) throws NoClientsFound, NotSpecifiedReqArgs {
+        Client client = (Client) clientService.findByUsername(principal.getName());
+        model.addAttribute("invoices", saleService.getSalesByClient(client));
+        return "list-invoices";
     }
 
     @GetMapping("/home")
