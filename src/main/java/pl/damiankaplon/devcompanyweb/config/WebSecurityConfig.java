67710a1d0db5781408c4491aae71fde571985de0
Damian <kaplon.damian99@gmail.com>
2021-12-19 16:42:04 +0100

    All requests authentication

--- src/main/java/pl/damiankaplon/devcompanyweb/config/WebSecurityConfig.java
+++ src/main/java/pl/damiankaplon/devcompanyweb/config/WebSecurityConfig.java
@@ -28,12 +28,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().disable();
         http.authorizeRequests()
-                .antMatchers("/start").authenticated()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin().defaultSuccessUrl("/hello");
+                .formLogin().defaultSuccessUrl("/home");
     }
 
     @Bean
--- src/main/java/pl/damiankaplon/devcompanyweb/controller/UserController.java
+++ src/main/java/pl/damiankaplon/devcompanyweb/controller/UserController.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @Controller
 public class UserController {
-    @GetMapping("start")
-    public String hello(){
-        return "hello";
-    }
-    @GetMapping("hello")
-    public String hello2(){
-        return "hello";
+    @GetMapping("/home")
+    public String login(){
+        return "home";
     }
 }
